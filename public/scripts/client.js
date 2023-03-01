4ea0d3a2e162a119a130b424d9146c64f72deab4
Liz Fosdick <lizfosdick@gmail.com>
2023-03-01 15:34:23 -0800

    Add renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,14 +6,17 @@
 
 const createTweetElement = function(tweet) {
   const $tweet = $("<article>").addClass("tweet");
+  //header variables
   const $tweetHeader = $("<header>");
   const $divLeftHeader = $("<div>").addClass("left-header");
   const $imgAvatar = $(`<img src='${tweet.user.avatars}'>`).addClass("left-header");
   const $pName = $("<p>").text(tweet.user.name);
   const $divHandleText = $("<div>").addClass("handle-text").text(tweet.user.handle);
   
+  //tweet text variables
   const $divTweetText = $("<div>").addClass("tweet-text").text(tweet.content.text);
 
+  //tweet footer variables
   const $tweetFooter = $("<footer>");
   const $divFooter = $("<div>").text(tweet.created_at);
   const $divFooterIcons = $("<div>").addClass("footer-icons");
@@ -24,16 +27,16 @@ const createTweetElement = function(tweet) {
   
   $tweet.append($tweetHeader);
 
-  //tweet header
+  //appending to the tweet header
   $tweetHeader.append($divLeftHeader);
   $divLeftHeader.append($imgAvatar);
   $divLeftHeader.append($pName);
   $tweetHeader.append($divHandleText);
 
-  //tweet text
+  //appending the tweet text
   $tweet.append($divTweetText);
 
-  //tweet footer
+  //appending to the tweet footer
   $tweet.append($tweetFooter);
   $tweetFooter.append($divFooter);
   $tweetFooter.append($divFooterIcons);
@@ -42,24 +45,48 @@ const createTweetElement = function(tweet) {
   $divFooterIcons.append($heartIcon);
 
 
-  $('#all-tweets').append($tweet); 
+  //$('#all-tweets-container').append($tweet); 
   return $tweet;
 }
 
+
+const renderTweets = function(tweets) {
+  for (let singleTweet of tweets) {
+    const newTweet = createTweetElement(singleTweet);
+    $('#all-tweets-container').append(newTweet);
+  }
+  return;
+};
+
+
 $(document).ready(function() {
   console.log("Document - clientjs - is ready");
-  const newTweet = createTweetElement(tweetData);
+  renderTweets(data);
 });
 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
