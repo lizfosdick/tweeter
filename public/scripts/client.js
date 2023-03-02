22fecf8aef58a4b5836643445120aadc64194793
Liz Fosdick <lizfosdick@gmail.com>
2023-03-01 16:49:45 -0800

    Add AJAX get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
 const createTweetElement = function(tweet) {
   const $tweet = $("<article>").addClass("tweet");
   //header variables
@@ -53,40 +55,35 @@ const createTweetElement = function(tweet) {
 const renderTweets = function(tweets) {
   for (let singleTweet of tweets) {
     const newTweet = createTweetElement(singleTweet);
-    $('#all-tweets-container').append(newTweet);
+    $('#all-tweets-container').prepend(newTweet);
   }
   return;
 };
 
-
 $(document).ready(function() {
   console.log("Document - clientjs - is ready");
-  renderTweets(data);
+
+  $( "#tweet-form" ).submit(function( event ) {
+    console.log("Handler for .submit() called")
+    event.preventDefault();
+    $.post( "/tweets", $( this ).serialize() );
+  })
+
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: (response) => {
+        renderTweets(response);
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
+  loadTweets();
 });
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
