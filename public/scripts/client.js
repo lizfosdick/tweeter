94d1ffef8aa6817d27d7f4033ff5899d1e60dfbb
Liz Fosdick <lizfosdick@gmail.com>
2023-03-02 17:24:58 -0800

    Code cleanup and final revisions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,71 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
 $(document).ready(function() {
-  console.log("Document - clientjs - is ready");
 
   $("#error").hide();
 
-//takes in a tweet object and returns a tweet <article> element
-const createTweetElement = function(tweet) {
+  //takes in a tweet object and returns a tweet <article> element
+  const createTweetElement = function(tweet) {
+
+    const $tweet = $("<article>").addClass("tweet");
+    //header variables
+    const $tweetHeader = $("<header>");
+    const $divLeftHeader = $("<div>").addClass("left-header");
+    const $imgAvatar = $(`<img src='${tweet.user.avatars}'>`).addClass("left-header");
+    const $pName = $("<p>").text(tweet.user.name);
+    const $divHandleText = $("<div>").addClass("handle-text").text(tweet.user.handle);
+    
+    //tweet text variables
+    const $divTweetText = $("<div>").addClass("tweet-text").text(tweet.content.text);
+    const date = new Date(tweet.created_at);
+
+    //tweet footer variables
+    const $tweetFooter = $("<footer>");
+    const $divFooter = $("<div>").html(timeago.format(date));
+    const $divFooterIcons = $("<div>").addClass("footer-icons");
+    const $flagIcon = $("<i>").addClass("fa-flag").addClass("fa-solid");
+    const $retweetIcon = $("<i>").addClass("fa-retweet").addClass("fa-solid").addClass("fa-sharp");
+    const $heartIcon = $("<i>").addClass("fa-heart").addClass("fa-solid");
   
+    $tweet.append($tweetHeader);
 
-  const $tweet = $("<article>").addClass("tweet");
-  //header variables
-  const $tweetHeader = $("<header>");
-  const $divLeftHeader = $("<div>").addClass("left-header");
-  const $imgAvatar = $(`<img src='${tweet.user.avatars}'>`).addClass("left-header");
-  const $pName = $("<p>").text(tweet.user.name);
-  const $divHandleText = $("<div>").addClass("handle-text").text(tweet.user.handle);
-  
-  //tweet text variables
-  const $divTweetText = $("<div>").addClass("tweet-text").text(tweet.content.text);
+    //appending to the tweet header
+    $tweetHeader.append($divLeftHeader);
+    $divLeftHeader.append($imgAvatar);
+    $divLeftHeader.append($pName);
+    $tweetHeader.append($divHandleText);
 
-  const date = new Date(tweet.created_at)
-  //tweet footer variables
-  const $tweetFooter = $("<footer>");
-  const $divFooter = $("<div>").html(timeago.format(date));
-  const $divFooterIcons = $("<div>").addClass("footer-icons");
-  const $flagIcon = $("<i>").addClass("fa-flag").addClass("fa-solid");
-  const $retweetIcon = $("<i>").addClass("fa-retweet").addClass("fa-solid").addClass("fa-sharp");
-  const $heartIcon = $("<i>").addClass("fa-heart").addClass("fa-solid");
- 
-  
-  $tweet.append($tweetHeader);
+    //appending the tweet text
+    $tweet.append($divTweetText);
 
-  //appending to the tweet header
-  $tweetHeader.append($divLeftHeader);
-  $divLeftHeader.append($imgAvatar);
-  $divLeftHeader.append($pName);
-  $tweetHeader.append($divHandleText);
+    //appending to the tweet footer
+    $tweet.append($tweetFooter);
+    $tweetFooter.append($divFooter);
+    $tweetFooter.append($divFooterIcons);
+    $divFooterIcons.append($flagIcon);
+    $divFooterIcons.append($retweetIcon);
+    $divFooterIcons.append($heartIcon);
 
-  //appending the tweet text
-  $tweet.append($divTweetText);
+    return $tweet;
+  };
 
-  //appending to the tweet footer
-  $tweet.append($tweetFooter);
-  $tweetFooter.append($divFooter);
-  $tweetFooter.append($divFooterIcons);
-  $divFooterIcons.append($flagIcon);
-  $divFooterIcons.append($retweetIcon);
-  $divFooterIcons.append($heartIcon);
-
-  return $tweet;
-}
-
-//renders all the tweets in the database
-const renderTweets = function(tweets) {
-  $("#all-tweets-container").empty();
-  for (let singleTweet of tweets) {
-    const newTweet = createTweetElement(singleTweet);
-    $('#all-tweets-container').prepend(newTweet);
-  }
-  //return;
-};
+  //renders all the tweets in the database
+  const renderTweets = function(tweets) {
+    $("#all-tweets-container").empty();
+    for (let singleTweet of tweets) {
+      const newTweet = createTweetElement(singleTweet);
+      $("#all-tweets-container").prepend(newTweet);
+    }
+  };
 
   //requests /tweets and receives array of tweets as JSON
   const loadTweets = () => {
@@ -76,50 +65,31 @@ const renderTweets = function(tweets) {
         renderTweets(response);
       },
       error: (err) => {
-        console.log(err)
+        console.log(err);
       }
-    })
-  }
- 
+    });
+  };
 
-  $( "#tweet-form" ).submit(function( event ) {
-    console.log("Handler for .submit() called")
-
+  //posts tweet or displays error
+  $("#tweet-form").submit(function(event) {
     event.preventDefault();
-    if ($( "#tweet-text" ).val() === "") {
-      $("#error").text("⚠️Tweet cannot be empty.⚠️").slideDown('slow').delay(2500).slideUp('slow');
+    if ($("#tweet-text").val() === "") {
+      $("#error").text("⚠️Tweet cannot be empty.⚠️").slideDown("slow").delay(2500).slideUp("slow");
       return;
-    } else if ($( "#tweet-text").val().length> 140) {
-      $("#error").text("Too many characters! Please shorten your tweet.✏️").slideDown('slow').delay(2500).slideUp('slow');
-        return;
-      } else {
-       // $.post( "/tweets", $( this ).serialize() );
-        $.ajax({
-          type: 'POST',
-          url: '/tweets',
-          data: $( this ).serialize(),
-          // success: (response) => {
-          //   renderTweets(response);
-          // },
-          // error: (err) => {
-          //   console.log(err)
-          // }
-        
-        }).then((result) => {
-          loadTweets();
-          $('#tweet-text').val("");
-          $('.counter').val(140);
-        })
-        
-      }
-    
-   
-    
-  })
+    } else if ($("#tweet-text").val().length > 140) {
+      $("#error").text("Too many characters! Please shorten your tweet.✏️").slideDown("slow").delay(2500).slideUp("slow");
+      return;
+    } else {
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: $(this).serialize()
+      }).then(() => {
+        loadTweets();
+        $('#tweet-text').val("");
+        $('.counter').val(140);
+      });
+    }
+  });
   loadTweets();
 });
-
-
-
-
-
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
-  console.log("Document is ready");
 
   //updates the character count in the new tweet section
-  $( "#tweet-text" ).on("keyup", function (event) {
+  $("#tweet-text").on("keyup", function() {
     let charCount = 140;
-    const pressedKey = $( this ).val().length;
+    const pressedKey = $(this).val().length;
     charCount -= pressedKey;
-    $( this ).parent().children().find(".counter").text(charCount);
-
-      if (charCount < 0) {
-        $( this ).parent().children().find(".counter").addClass("red");
-      }
-    
-
+    $(this).parent().children().find(".counter").text(charCount);
+    if (charCount < 0) {
+      $(this).parent().children().find(".counter").addClass("red");
+    }
   });
 });
 
